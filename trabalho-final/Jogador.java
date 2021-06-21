1d1cea6c5085c7d779b5d01783c7a7c97aed6ee1
Maria Eduarda Krutzsch <62608046+mariaedk@users.noreply.github.com>
2021-06-20 21:18:22 -0300

    Add files via upload

---
+++ trabalho-final/Jogador.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Jogador {
+    // atributo mapa para poder usar no metodo jogar
+    Mapa mapa;
+    private char letra = 'X';
+
+    public Jogador(Mapa mapa){
+
+        this.mapa = mapa; 
+    }
+
+    public boolean jogar(Scanner teclado){ 
+
+        boolean jogadaJogador;
+        int l; int c;
+
+        do {
+            System.out.println("Jogador ..");
+            System.out.print("  linha: ");
+            l = teclado.nextInt();
+            System.out.print("  coluna: ");
+            c = teclado.nextInt();
+            
+        } while ((l < 0 || l > 2) || (c < 0 || c > 2));
+
+        jogadaJogador = mapa.jogar(l, c, letra);
+ 
+        while (jogadaJogador == false){  
+                System.out.println("posição inválida!");
+                System.out.println("Jogador ..");
+                System.out.print("  linha: ");
+                l = teclado.nextInt();
+                System.out.print("  coluna: ");
+                c = teclado.nextInt();
+ 
+                if ((l < 0 || l > 2) || (c < 0 || c > 2)){
+                    System.out.println("Jogador ..");
+                    System.out.print("  linha: ");
+                    l = teclado.nextInt();
+                    System.out.print("  coluna: ");
+                    c = teclado.nextInt();
+                }
+ 
+                jogadaJogador = mapa.jogar(l, c, letra);
+            }
+ 
+        boolean naoVenceu = mapa.verificarGanhador('X');
+        if (naoVenceu == false){
+            System.out.println("... Jogador GANHOU!");
+            return false;
+        }
+        return true;
+    }
+}
